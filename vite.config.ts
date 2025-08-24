771710bbb39671f03136f65a32621e1ab9943b59
jirishus <jirishus@yahoo.com>
2025-08-24 11:31:54 -0700

    updates for static assets

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,28 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name?.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType || '')) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+  },
+  server: {
+    headers: {
+      'Content-Type': 'text/javascript; charset=utf-8',
+    },
+  },
 });
